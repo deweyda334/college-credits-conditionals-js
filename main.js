d7bacd8c3b7dd1c8e389f46153615050f0e3b38e
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-05-22 12:57:53 -0400

    Updated script for the conditionals project

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const requiredCredits = 120;
+const current = Number(prompt('Enter the number of credits you have today:\n'));
+if (current >= requiredCredits) {
+    console.log("you have enough credits to graduate.");
+} else if (current <= 90) {
+  console.log("not enough");
+  console.log(`Today you have this many credits: ${current}`);
+  console.log(`you still need ${requiredCredits - current} credits to graduate.`);
+} else {
+  console.log("You have a long way to go before you have enough credits to graduate!");
+  console.log(`Current number of credits: ${current}`);
+  console.log(`You need need ${requiredCredits - current} credits before you can graduate.`);
+} 
